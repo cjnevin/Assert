b37ef494ee68214ead8c247f6c19a96b5d030aea
Chris Nevin <chris.nevin@tigerspike.com>
2022-11-01 07:42:10 +1000

    Initial release.

---
+++ Sources/Assert/Assert.swift
@@ -0,0 +1,189 @@
+import XCTest
+
+public protocol OptionalType {
+    associatedtype Wrapped
+    var unwrapped: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+    public var unwrapped: Wrapped? {
+        switch self {
+        case .some(let value): return value
+        case .none: return nil
+        }
+    }
+}
+
+public func assert<T>(_ value: @autoclosure () throws -> T) rethrows -> Assert<T> {
+    try .init(value())
+}
+
+struct DidNotThrowError: Error {}
+public func assertThrows<T>(_ closure: @autoclosure () throws -> T, file: StaticString = #file, line: UInt = #line) rethrows {
+    do {
+        _ = try closure()
+        throw DidNotThrowError()
+    } catch {
+
+    }
+}
+
+public func scope<T>(_ scope: T, _ closure: (Assert<T>) throws -> Void) rethrows {
+    try closure(assert(scope))
+}
+
+public func unwrap<T: OptionalType>(_ value: T) throws -> Assert<T.Wrapped> {
+    try assert(value).unwrap()
+}
+
+@dynamicMemberLookup
+public struct Assert<T> {
+    public let value: T
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+    public subscript<Value>(dynamicMember keyPath: KeyPath<T, Value>) -> Assert<Value> {
+        assert(value[keyPath: keyPath])
+    }
+
+    public func scope(_ closure: @escaping (Self) throws -> Void) rethrows {
+        try closure(self)
+    }
+
+    public func map<U>(_ closure: (T) -> U) -> Assert<U> {
+        .init(closure(value))
+    }
+
+    public func explodes<T>(_ closure: @escaping (Self) throws -> T, file: StaticString = #file, line: UInt = #line) rethrows {
+        XCTAssertThrowsError(try closure(self), file: file, line: line)
+    }
+}
+
+extension Assert where T: Sequence, T.Element: Comparable {
+    public func sorted() -> Assert<[T.Element]> {
+        .init(value.sorted())
+    }
+}
+
+extension Assert where T: OptionalType {
+    public func isNil(file: StaticString = #file, line: UInt = #line) {
+        XCTAssertNil(value.unwrapped, file: file, line: line)
+    }
+
+    public func notNil(file: StaticString = #file, line: UInt = #line) {
+        XCTAssertNotNil(value.unwrapped, file: file, line: line)
+    }
+
+    public func unwrap(file: StaticString = #file, line: UInt = #line) throws -> Assert<T.Wrapped> {
+        .init(try XCTUnwrap(value.unwrapped, file: file, line: line))
+    }
+}
+
+extension Assert where T: AnyObject {
+    public func identical(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertIdentical(value, other, file: file, line: line)
+    }
+
+    public func notIdentical(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertNotIdentical(value, other, file: file, line: line)
+    }
+}
+
+extension Assert where T == Bool {
+    public func isTrue(file: StaticString = #file, line: UInt = #line) {
+        XCTAssertTrue(value, file: file, line: line)
+    }
+
+    public func isFalse(file: StaticString = #file, line: UInt = #line) {
+        XCTAssertFalse(value, file: file, line: line)
+    }
+}
+
+extension Assert where T: Equatable {
+    public func equal(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(value, other, file: file, line: line)
+    }
+
+    public func notEqual(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertNotEqual(value, other, file: file, line: line)
+    }
+}
+
+extension Assert where T: Comparable {
+    public func greater(than other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertGreaterThan(value, other, file: file, line: line)
+    }
+
+    public func greaterThanOrEqual(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertGreaterThanOrEqual(value, other, file: file, line: line)
+    }
+
+    public func less(than other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertLessThan(value, other, file: file, line: line)
+    }
+
+    public func lessThanOrEqual(to other: T, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertLessThanOrEqual(value, other, file: file, line: line)
+    }
+
+    public func within(_ range: ClosedRange<T>, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertTrue(range ~= value, file: file, line: line)
+    }
+
+    public func outsideOf(_ range: ClosedRange<T>, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertFalse(range ~= value, file: file, line: line)
+    }
+}
+
+public func === <T: AnyObject>(lhs: Assert<T>, rhs: T) {
+    lhs.identical(to: rhs)
+}
+
+public func !== <T: AnyObject>(lhs: Assert<T>, rhs: T) {
+    lhs.notIdentical(to: rhs)
+}
+
+public func == <T: Equatable>(lhs: Assert<T>, rhs: T) {
+    lhs.equal(to: rhs)
+}
+
+public func != <T: Equatable>(lhs: Assert<T>, rhs: T) {
+    lhs.notEqual(to: rhs)
+}
+
+public func < <T: Comparable>(lhs: Assert<T>, rhs: T) {
+    lhs.less(than: rhs)
+}
+
+public func <= <T: Comparable>(lhs: Assert<T>, rhs: T) {
+    lhs.lessThanOrEqual(to: rhs)
+}
+
+public func > <T: Comparable>(lhs: Assert<T>, rhs: T) {
+    lhs.greater(than: rhs)
+}
+
+public func >= <T: Comparable>(lhs: Assert<T>, rhs: T) {
+    lhs.greaterThanOrEqual(to: rhs)
+}
+
+public func ~= <T: Comparable>(lhs: ClosedRange<T>, rhs: Assert<T>) {
+    rhs.within(lhs)
+}
+
+infix operator !~=: ComparisonPrecedence
+public func !~= <T: Comparable>(lhs: ClosedRange<T>, rhs: Assert<T>) {
+    rhs.outsideOf(lhs)
+}
+
+infix operator =~: ComparisonPrecedence
+public func =~ <T: Comparable>(lhs: Assert<T>, rhs: ClosedRange<T>) {
+    lhs.within(rhs)
+}
+
+infix operator !=~: ComparisonPrecedence
+public func !=~ <T: Comparable>(lhs: Assert<T>, rhs: ClosedRange<T>) {
+    lhs.outsideOf(rhs)
+}
---
+++ Tests/AssertTests/AssertTests.swift
@@ -0,0 +1,95 @@
+import XCTest
+@testable import Assert
+
+final class AssertTests: XCTestCase {
+    let obj = User.Test()
+    lazy var user = User(
+        object: obj,
+        name: "test",
+        id: 1,
+        optional: nil,
+        prefs: .init(email: true, phone: true),
+        array: ["a", "b"],
+        dictionary: ["a": 1, "b": 2]
+    )
+
+    func testReferenceType() {
+        assert(user.object) === obj
+        assert(user.object) !== .init()
+    }
+
+    func testDynamicMemberLookup() {
+        assert(user.prefs.email) == true
+        assert(user.array.isEmpty) == false
+    }
+
+    func testComparable() {
+        scope(user.id) {
+            $0 > 0
+            $0 >= 0
+            $0 < 2
+            $0 <= 2
+            $0 =~ 0...2
+            0...2 ~= $0
+            $0 !=~ 2...4
+            2...4 !~= $0
+        }
+    }
+
+    func testAssertThrows() throws {
+        struct UnwrapError: Error {}
+        assertThrows(UnwrapError())
+
+        func test() throws {
+            throw UnwrapError()
+        }
+        try assertThrows(test())
+    }
+
+    func testMap() {
+        assert(user.name).map { String($0.reversed()) } == "tset"
+    }
+
+    func testSorted() {
+        assert(user.dictionary.keys.sorted()) == ["a", "b"]
+        assert(user.dictionary.values.sorted()) == [1, 2]
+    }
+
+    func testOptional() throws {
+        var copy = user
+        assert(copy.optional) == nil
+        assert(copy.optional).isNil()
+
+        copy.optional = "abc"
+        assert(copy.optional) == "abc"
+        assert(copy.optional).notNil()
+
+        try assert(copy.optional).unwrap() == "abc"
+    }
+
+    func testScope() {
+        scope(user.prefs) {
+            $0.email == true
+            $0.phone == true
+        }
+        assert(user.prefs).scope {
+            $0.email == true
+            $0.phone == true
+        }
+    }
+}
+
+struct User {
+    struct Prefs: Equatable {
+        var email: Bool
+        var phone: Bool
+    }
+    class Test {}
+    var object: Test
+    var name: String
+    var id: Int
+    var optional: String?
+    var prefs: Prefs
+    var array: [String]
+    var dictionary: [String: Int]
+}
